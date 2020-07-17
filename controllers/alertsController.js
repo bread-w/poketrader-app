02cb05dd1c0d9810bccb037c13c7255cc85e6b50
Brad Williams <jb.wms89@gmail.com>
2020-07-17 09:55:27 -0400

    created initial skeleton

---
+++ controllers/alertsController.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+// /api/alerts/
+router.post("/", (req, res) => {
+  db.Alert.create(req.body)
+    .then((result) => {
+      res.json({
+        error: false,
+        data: result,
+        message: "Successfully created new alert",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to create new alert.",
+      });
+    });
+});
+
+// /api/alerts/:id
+router.put("/:id", (req, res) => {
+  res.json({
+    message: "Put route",
+  });
+});
+
+// /api/alerts/:id
+router.delete("/:id", (req, res) => {
+  res.json({
+    message: "Delete route",
+  });
+});
+
+module.exports = router;
