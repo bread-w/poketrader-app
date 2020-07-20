21f97f2a0ca614f6b280ec5997f03559e9f98e22
Dwight Huggins <dwighthuggins@DwightHugginsJr.local>
2020-07-20 10:39:33 -0400

    modified files

--- public/assets/js/add_card.js
+++ public/assets/js/add_card.js
@@ -1,35 +1,55 @@
-const mainUrl = 'https://api.pokemontcg.io/v1/cards?name=';
-var searchEl = document.getElementsByClassName("search-pokemon")[0].value;
-const pokemonName = document.querySelector(".pokemon-name")
-
-
-
-//listener to movie button
- $("#search-pokemon-btn").on("click", function (event) {
-  event.preventDefault();
+$(document).ready(function () {
+  const mainUrl = "https://api.pokemontcg.io/v1/cards?name=";
   var searchEl = document.getElementsByClassName("search-pokemon")[0].value;
+  var pokeInput = $("button.pokemonCards");
 
-  console.log("I've been clicked!");
+  //listener to movie button
+  $("#search-pokemon-btn").on("click", function (event) {
+    event.preventDefault();
+    var searchEl = document.getElementsByClassName("search-pokemon")[0].value;
 
-  searchPokemon(searchEl);
-}); 
+    console.log("I've been clicked!");
 
-// Not working yet. Trying to pull the the value of the ID from the card chosen
-$(document).on("click", ".pokemonCards", function (event) {
-  event.preventDefault();
-  var pokemonValue = $(this).attr("id");
-  // const pokemonValue = document.getElementById(this).value;
-  console.log(pokemonValue);
-})
+    searchPokemon(searchEl);
+  });
 
-// call function
- function searchPokemon(value){
-   $.get("add-card",{
-     name: value,
-   })
-   .then(function(response){
-     
-     console.log(response);
-     window.location.replace("/add-card/" + value);
-   })
- };
+  pokeInput.on("click", function (event) {
+    var pokemonValue = $(this).attr("id");
+    var pokemonImg = $(this).attr("src");
+
+    var cardData = {
+      card_code: pokemonValue,
+      card_img: pokemonImg,
+    };
+    console.log(pokemonImg);
+    console.log(cardData);
+    makeNewCard(cardData.card_code, cardData.card_img);
+  });
+
+  $(".big").click(function(){
+    var ids = $(this).children().map(function(){
+      return this.id;
+    }).toArray();
+    console.log(ids);
+  });
+
+  function makeNewCard(pokemonValue, pokemonImg) {
+    $.post("/cards", {
+      card_code: pokemonValue,
+      card_img: pokemonImg,
+    }).then(function (data) {
+      console.log(data);
+      /* window.location.replace("/add-card"); */
+    });
+  }
+
+  // call function
+  function searchPokemon(value) {
+    $.get("add-card", {
+      name: value,
+    }).then(function (response) {
+      console.log(response);
+      window.location.replace("/add-card/" + value);
+    });
+  }
+});
