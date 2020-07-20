381d3bbb8eec7c27118af4d061149baf351674b1
Justin <ponthierjustin@gmail.com>
2020-07-19 20:51:01 -0400

    fixing ids and adding post for sign up pages first and lastname

--- public/assets/js/sign_up.js
+++ public/assets/js/sign_up.js
@@ -2,27 +2,35 @@ $(document).ready(function () {
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
+  var firstInput = $("input#first-name");
+  var lastInput = $("input#last-name");
 
   signUpForm.on("submit", function (event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim(),
+      firstName: firstInput.val().trim(),
+      lastName: lastInput.val().trim(),
     };
 
     if (!userData.email || !userData.password) {
       return;
     }
 
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.email, userData.password, userData.firstName, userData.lastName);
     emailInput.val("");
     passwordInput.val("");
+    firstInput.val("");
+    lastInput.val("");
   });
 
-  function signUpUser(email, password) {
+  function signUpUser(email, password, firstName, lastName) {
     $.post("/users", {
       email: email,
       password: password,
+      firstName: firstName,
+      lastName: lastName,
     })
       .then(function (data) {
         console.log(data)
