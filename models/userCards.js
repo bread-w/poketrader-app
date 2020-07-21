6864ac4c7443326c770db5277e4c217ee922bafd
Brad Williams <jb.wms89@gmail.com>
2020-07-21 10:31:05 -0400

    restarting computer because of some server error

--- models/userCards.js
+++ models/userCards.js
@@ -2,6 +2,7 @@ module.exports = function (sequelize, DataTypes) {
   const UserCard = sequelize.define("UserCard", {
     card_id: DataTypes.STRING,
     user_id: DataTypes.INTEGER,
+    card_img: DataTypes.STRING,
   });
   // update to ES6
 
