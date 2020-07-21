44968ac69aa10e0b87e8ca34421c02af9f971a6f
Brad Williams <jb.wms89@gmail.com>
2020-07-21 13:08:39 -0400

    added more styling elements to the page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
 
 router.get("/api/config", (req, res) => {
   res.json({
     success: true,
+  });
+});
+
+router.delete("/api/usercards/:id", function (req, res) {
+  // We just have to specify which todo we want to destroy with "where"
+  db.UserCard.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(function (response) {
+    res.json(response);
   });
 });
 
